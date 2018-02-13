9395592b19856d22926955a061ca76dec403e620
= <rightgear11@gmail.com>
2018-02-13 17:49:47 +0900

    Add some java files for DP solutions

---
+++ dp/boj_1149.java
@@ -0,0 +1,27 @@
+package practice;
+
+import java.util.Scanner;
+
+// RGB 거리
+public class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int input = sc.nextInt();
+		int[][] grid = new int[input + 1][3 + 1];
+		for (int i = 1; i <= input; i++) {
+			for (int j = 1; j <= 3; j++) {
+				grid[i][j] = sc.nextInt();
+			}
+		}
+		int[][] dp = new int[input + 1][3 + 1];
+		for (int i = 1; i <= 3; i++) {
+			dp[0][i] = 0;
+		}
+		for (int i = 1; i <= input; i++) {
+			dp[i][1] = Math.min(dp[i - 1][2], dp[i - 1][3]) + grid[i][1];
+			dp[i][2] = Math.min(dp[i - 1][1], dp[i - 1][3]) + grid[i][2];
+			dp[i][3] = Math.min(dp[i - 1][1], dp[i - 1][2]) + grid[i][3];
+		}
+		System.out.println(Math.min(Math.min(dp[input][1], dp[input][2]), dp[input][3]));
+	}
+}
---
+++ dp/boj_2096.java
@@ -0,0 +1,32 @@
+package practice;
+
+import java.util.Scanner;
+
+// 내려가기
+public class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int input = sc.nextInt();
+		int[][] grid = new int[input + 1][3 + 1];
+		for (int i = 1; i <= input; i++) {
+			for (int j = 1; j <= 3; j++) {
+				grid[i][j] = sc.nextInt();
+			}
+		}
+		int[][] dpMax = new int[input + 1][3 + 1];
+		int[][] dpMin = new int[input + 1][3 + 1];
+		for (int i = 1; i <= input; i++) {
+			dpMax[i][1] = Math.max(dpMax[i - 1][1], dpMax[i - 1][2]) + grid[i][1];
+			dpMax[i][2] = Math.max(dpMax[i - 1][3], Math.max(dpMax[i - 1][1], dpMax[i - 1][2])) + grid[i][2];
+			dpMax[i][3] = Math.max(dpMax[i - 1][2], dpMax[i - 1][3]) + grid[i][3];
+			
+			dpMin[i][1] = Math.min(dpMin[i - 1][1], dpMin[i - 1][2]) + grid[i][1];
+			dpMin[i][2] = Math.min(dpMin[i - 1][3], Math.min(dpMin[i - 1][1], dpMin[i - 1][2])) + grid[i][2];
+			dpMin[i][3] = Math.min(dpMin[i - 1][2], dpMin[i - 1][3]) + grid[i][3];
+		}
+		
+		int max = Math.max(Math.max(dpMax[input][1], dpMax[input][2]), dpMax[input][3]);
+		int min = Math.min(Math.min(dpMin[input][1], dpMin[input][2]), dpMin[input][3]);
+		System.out.println(max + " " + min);
+	}
+}
