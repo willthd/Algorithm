414e5c6adae3b4e9dc031dce0c2ace2505277ab2
= <rightgear11@gmail.com>
2018-02-24 23:37:52 +0900

    Modify

--- dp/boj_1149.java
+++ dp/boj_1149.java
@@ -11,7 +11,7 @@ public class Solution {
 		for (int i = 1; i <= input; i++) {
 			for (int j = 1; j <= 3; j++) {
 				grid[i][j] = sc.nextInt();
-			}
+			} 
 		}
 		int[][] dp = new int[input + 1][3 + 1];
 		for (int i = 1; i <= 3; i++) {
