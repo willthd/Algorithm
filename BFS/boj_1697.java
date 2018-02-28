47e1d303ae071a99d92dd57f48c3d8688b32385b
= <rightgear11@gmail.com>
2018-02-28 15:47:14 +0900

    숨바꼭질

--- BFS/boj_1697.java
+++ BFS/boj_1697.java
@@ -2,7 +2,7 @@ package practice;
 import java.util.*;
 
 // 숨바꼭질
-class Main2 {
+class Main {
 	static int d[] = new int[100001];
 
 	public static void main(String[] args) {
@@ -10,23 +10,24 @@ class Main2 {
 
 		int N = sc.nextInt();
 		int K = sc.nextInt();
-		
+
 		Queue<Integer> q = new LinkedList<>();
 		q.add(N);
-		d[N] = 1;
-		
+		Arrays.fill(d, -1);
+		d[N] = 0;
+
 		while(!q.isEmpty()) {
 			int cur = q.poll();
 			int next[] = { cur - 1, cur + 1, cur * 2 };
 			for (int i = 0; i < 3; i++) {
-				if (0 <= next[i] && next[i] <= 100) {
-					if (d[next[i]] == 0) {
+				if (0 <= next[i] && next[i] <= 100000) {
+					if (d[next[i]] == -1) {
 						d[next[i]] = d[cur] + 1;
 						q.add(next[i]);
 					}
 				}
 			}
 		}
-		System.out.println(d[K] - 1);
+		System.out.println(d[K]);
 	}
 }
