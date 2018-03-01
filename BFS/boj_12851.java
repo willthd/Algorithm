8f9cd9ff2030a63a1337e6f6d7c75970a53493be
= <rightgear11@gmail.com>
2018-03-01 17:42:35 +0900

    숨바꼭질2

--- BFS/boj_12851.java
+++ BFS/boj_12851.java
@@ -1,10 +1,12 @@
 package practice;
 import java.util.*;
 
-// 숨바꼭질2
+// 숨바꼭질2, 숨바꼭질1과 다르게 visited[] 추가했다. 꼭 필요하지 않지만 이런 형태를 기억하는 것이 좋을 것 같다
 class Main2 {
-	static int d[] = new int[100001];
-	static int c[] = new int[100001];
+	public static final int MAX = 100001;
+	static int dist[] = new int[MAX];
+	static int count[] = new int[MAX];
+	static boolean visited[] = new boolean[MAX];
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -14,28 +16,28 @@ class Main2 {
 		
 		Queue<Integer> q = new LinkedList<>();
 		q.add(N);
-		Arrays.fill(d, -1);
-		d[N] = 0;
-		c[N] = 1;
+		visited[N] = true;
+		count[N] = 1;
 		
 		while(!q.isEmpty()) {
 			int cur = q.poll();
 			int next[] = { cur - 1, cur + 1, cur * 2 };
-			for (int i = 0; i < 3; i++) {
+			for (int i = 0; i < next.length; i++) {
 				if (0 <= next[i] && next[i] <= 100000) {
-					if (d[next[i]] == -1) {
-						d[next[i]] = d[cur] + 1;
-						c[next[i]] = c[cur];
+					if (!visited[next[i]]) {
+						dist[next[i]] = dist[cur] + 1;
+						count[next[i]] = count[cur];
+						visited[next[i]] = true;
 						q.add(next[i]);
-					} else if (d[next[i]] == d[cur] + 1) {
-						c[next[i]] = c[cur] + 1;
-//						c[next[i]]++; 안되는 이유는? 정확히 안되는 테스트 케이스 못찾겠는데,
-//						최대한 이전 기준으로 증감을 표시하는 것이 좋을 것 같다
+					} else if (dist[next[i]] == dist[cur] + 1) {
+						count[next[i]] += count[cur];
+//						count[next[i]]++; or count[next[i] = count[cur] + 1 안되는 이유는?
+//						정확히 안되는 테스트 케이스 못찾겠는데, 최대한 이전 기준으로 증감을 표시하는 것이 좋을 것 같다
 					}
 				}
 			}
 		}
-		System.out.println(d[K]);
-		System.out.println(c[K]);
+		System.out.println(dist[K]);
+		System.out.println(count[K]);
 	}
 }
