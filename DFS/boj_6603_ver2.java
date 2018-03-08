7d6cadaa885a337cd93dff1ad143263ca1005222
= <rightgear11@gmail.com>
2018-03-09 03:10:03 +0900

    백준, 6603, 로또, ver2

--- DFS/boj_6603_ver2.java
+++ DFS/boj_6603_ver2.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 // 백준, 6603, 로또, ver2
 // ver1과 비교해봐라 DFS back tracking 부분
 // DFS + back tracking. 결국엔 DFS 메소드로 전부 해결나는 문제. 근데 이거 왜이렇게 생각이 안떠오르는거지...
+// 일단 DFS 함수는 매개변수로 index는 꼭 들고 있어야한다. cnt는 들고 있는 경우와 그렇지 않은 경우 있음. 어떤게 더 좋은 방법일까?
 public class Main {
 	static int n;
 	static int[] a;
