9a164dabbbf3f9941eaddfb46d5c75ca768db0b1
= <rightgear11@gmail.com>
2018-03-13 00:46:17 +0900

    백준, 5014, 스타트링크

--- BFS/boj_5014.java
+++ BFS/boj_5014.java
@@ -5,10 +5,10 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
-// 스타트링크
+// 백준, 5014, 스타트링크
 public class Main {
 	static int[] d = new int[1000001];
-	
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int full = sc.nextInt();
@@ -16,12 +16,12 @@ public class Main {
 		int target = sc.nextInt();
 		int up = sc.nextInt();
 		int down = sc.nextInt();
-		
+
 		Queue<Integer> q = new LinkedList<>();
 		q.add(now);				//enQueue
 		Arrays.fill(d, -1);		// -1로 초기화시킨다
 		d[now] = 0;				// 시작점
-		
+
 		while(!q.isEmpty()) {
 			int current = q.poll();		//deQueue
 			int[] canFloors = {current + up, current - down};
@@ -37,7 +37,7 @@ public class Main {
 		if (d[target] == -1) {
 			System.out.println("use the stairs");
 		} else {
-			System.out.println(d[target]);			
+			System.out.println(d[target]);
 		}
 	}
 }
