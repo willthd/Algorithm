2009c8c10b7ad8be434222a00755a4d2eb47f35e
= <rightgear11@gmail.com>
2018-03-15 14:05:59 +0900

    codeUp, 2608, 동아리 회장 선거

---
+++ DFS/codeUP_2608.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+// codeUp, 2608, 동아리 회장 선거
+public class Main {
+	static int n;
+	static void print(int cnt, String ans) {
+		if (cnt == n) {
+			System.out.println(ans);
+			return;
+		}
+		print(cnt + 1, ans + "O");
+		print(cnt + 1, ans + "X");
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		print(0, "");
+	}
+}
