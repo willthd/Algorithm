d32c26756821a98699f568c3780a8d2f87d72718
= <rightgear11@gmail.com>
2018-03-21 17:56:04 +0900

    SWEA, 4013, 특이한 자석

---
+++ recursion/swea_4013.java
@@ -0,0 +1,79 @@
+package practice;
+
+import java.util.*;
+
+// SWEA, 4013, 특이한 자석
+public class Main2 {
+	static int gear[][], dir[];
+	static boolean checked[];
+	
+	static void check(int num, int direction) {
+		if (checked[num]) {
+			return;
+		}
+		checked[num] = true;
+		dir[num] = direction;
+		if (num + 1 <= 3) {			
+			if (gear[num][2] != gear[num + 1][6]) {
+				check(num + 1, (-1) * direction);			
+			}
+		}
+		if (num - 1 >= 0) {
+			if (gear[num][6] != gear[num - 1][2]) {
+				check(num - 1, (-1) * direction);			
+			}
+		}
+		return;
+ 	}
+	
+	static void spin() {
+		for (int i = 0; i < 4; i++) {
+			if (dir[i] == 1) {
+				int temp = gear[i][7];
+				for (int j = 0; j < 8 - 1; j++) {
+					gear[i][7 - j] = gear[i][6 - j];
+				}
+				gear[i][0] = temp;
+			} else if (dir[i] == -1) {
+				int temp = gear[i][0];
+				for (int j = 0; j < 8 - 1; j++) {
+					gear[i][j] = gear[i][j + 1];
+				}
+				gear[i][7] = temp;
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+//		int n = sc.nextInt();
+//		for (int i = 0; i < n; i++) {
+			int m = sc.nextInt();
+			gear = new int[4][8];
+			for (int j = 0; j < 4; j++) {
+				for (int k = 0; k < 8; k++) {
+					gear[j][k] = sc.nextInt();
+				}
+			}
+			dir = new int[4];
+			checked = new boolean[4];
+			for (int j = 0; j < m; j++) {
+				int num = sc.nextInt() - 1;
+				int direction = sc.nextInt();
+				check(num, direction);	
+				for (int i = 0; i < 4; i++) {
+					System.out.print(dir[i] + " /");
+				}
+				System.out.println();
+				spin();
+				for (int i = 0; i < 4; i++) {
+					for (int k = 0; k < 8; k++) {
+						System.out.print(gear[i][k] + " ");
+					}
+					System.out.println();
+				}
+				System.out.println("***********");
+			}
+//		}
+	}
+}
