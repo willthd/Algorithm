9a18fd495519810092aaddc1cd5a6691f1071c00
= <rightgear11@gmail.com>
2018-03-25 23:49:10 +0900

    codeUp, 3530, 스도쿠

--- DFS/codeUp_3530.java
+++ DFS/codeUp_3530.java
@@ -5,45 +5,46 @@ import java.util.*;
 // CodeUp, 3530, 스도쿠
 public class Main5 {
 	static int a[][];
-	static int check1[][], check2[][], check3[][];
+	static boolean check1[][], check2[][], check3[][];
 	static int x[], y[], zeroNum;
 
-	static int func(int w) {
-		if (w == zeroNum) {
+	static void func(int dep) {
+		if (dep == zeroNum) {
 			for (int i = 0; i < 9; i++) {
 				for (int j = 0; j < 9; j++) {
 					System.out.print(a[i][j] + " ");
 				}
 				System.out.println();
 			}
-			return 1;
+			return;
 		}
 		for (int i = 1; i <= 9; i++) {
-			int xx = x[w], yy = y[w];
-			if (check1[xx][i] == 1 || check2[yy][i] == 1 || check3[xx / 3 * 3 + yy / 3][i] == 1) {
+			int xx = x[dep], yy = y[dep];
+			if (check1[xx][i] || check2[yy][i] || check3[xx / 3 * 3 + yy / 3][i]) {
 				continue;
 			}
-			check1[xx][i] = check2[yy][i] = check3[xx / 3 * 3 + yy / 3][i] = 1;
+			check1[xx][i] = check2[yy][i] = check3[xx / 3 * 3 + yy / 3][i] = true;
 			a[xx][yy] = i;
-			if (func(w + 1) == 1) {
-				return 1;
-			}
-			check1[xx][i] = check2[yy][i] = check3[xx / 3 * 3 + yy / 3][i] = 0;
+//			이렇게 해도 되는데 굳이 int형 return하는 것인가?
+//			if (func(dep + 1) == 1) {
+//				return 1;
+//			}
+			func(dep + 1);
+			check1[xx][i] = check2[yy][i] = check3[xx / 3 * 3 + yy / 3][i] = false;
 		}
-		return 0;
 	}
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int i, j;
 		a = new int[10][10];
-		check1 = new int[10][10];
-		check2 = new int[10][10];
-		check3 = new int[10][10];
+		check1 = new boolean[10][10];
+		check2 = new boolean[10][10];
+		check3 = new boolean[10][10];
 		x = new int[100];
 		y = new int[100];
 		zeroNum = 0;
-		for (i = 0; i < 9; i++)
+		for (i = 0; i < 9; i++) {
 			for (j = 0; j < 9; j++) {
 				a[i][j] = sc.nextInt();
 				if (a[i][j] == 0) {
@@ -51,11 +52,12 @@ public class Main5 {
 					y[zeroNum] = j;
 					zeroNum++;
 				} else {
-					check1[i][a[i][j]] = 1;
-					check2[j][a[i][j]] = 1;
-					check3[i / 3 * 3 + j / 3][a[i][j]] = 1;
+					check1[i][a[i][j]] = true;
+					check2[j][a[i][j]] = true;
+					check3[i / 3 * 3 + j / 3][a[i][j]] = true;
 				}
 			}
-		func(0);
+		}
+		func(0);			
 	}
 }
