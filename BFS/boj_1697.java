b10f326ed809e7efa52ddbb0f0fd2997187d80ba
= <rightgear11@gmail.com>
2018-03-27 21:21:44 +0900

    백준, 1697, 숨바꼭질

--- BFS/boj_1697.java
+++ BFS/boj_1697.java
@@ -1,34 +1,33 @@
 package practice;
+
 import java.util.*;
 
 // 백준, 1697, 숨바꼭질
-// 스타트링크와 비슷
-class Main {
-	static int d[] = new int[100001];
-
+public class Main7 {
+	static int n, k, dist[];
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-
-		int N = sc.nextInt();
-		int K = sc.nextInt();
-
+		n = sc.nextInt();
+		k = sc.nextInt();
+		dist = new int[100001];
+		Arrays.fill(dist, -1);
+		dist[n] = 0;
 		Queue<Integer> q = new LinkedList<>();
-		q.add(N);
-		Arrays.fill(d, -1);
-		d[N] = 0;
-
+		q.add(n);
 		while(!q.isEmpty()) {
-			int cur = q.poll();
-			int next[] = { cur - 1, cur + 1, cur * 2 };
+			int now = q.poll();
+			int next[] = {now - 1, now + 1, now * 2};
 			for (int i = 0; i < 3; i++) {
-				if (0 <= next[i] && next[i] <= 100000) {
-					if (d[next[i]] == -1) {
-						d[next[i]] = d[cur] + 1;
-						q.add(next[i]);
-					}
+				if (next[i] < 0 ||  next[i] > 100000) {
+					continue;
 				}
+				if (dist[next[i]] != -1) {
+					continue;
+				}
+				dist[next[i]] = dist[now] + 1;
+				q.add(next[i]);
 			}
 		}
-		System.out.println(d[K]);
+		System.out.println(dist[k]);
 	}
 }
