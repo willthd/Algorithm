2596e6e9ed8e57ec02bc093097633e10851ae969
= <rightgear11@gmail.com>
2018-03-28 12:07:20 +0900

    백준, 1260, BFS와 DFS

--- BFS/boj_1260.java
+++ BFS/boj_1260.java
@@ -1,65 +1,59 @@
 package practice;
 
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
+import java.util.*;
 
-// 백준, 1260, DFS와 BFS
-public class Solution {
-	static int[][] graph = new int[1001][1001];
-	static boolean[] visited = new boolean[1001];
-	static int node;
-	static int edge;
-	static int start;
+// 백준, 1260, BFS와 DFS
+// BFS와 DFS for문 내 구조 같다. 다만 BFS는 q를 이용하고, DFS는 재귀 호출을 할 뿐
+public class Main4 {
+	static int g[][], n, start;
+	static boolean visited[];
 	
-	public static void dfs(int start) {
-        visited[start] = true;
-        System.out.print(start + " ");
-
-        for(int i = 1; i <= node; i++){
-            if(graph[start][i] == 1 && visited[i] == false){
-                dfs(i);
-            }
-        }
-    }
-
-	public static void bfs(int start) {
-        Queue<Integer> q = new LinkedList<Integer>();
-        q.offer(start);
-        visited[start] = true;
-        System.out.print(start + " ");
-
-        int temp;
-        while(!q.isEmpty()){
-            temp = q.poll();
-            for(int i = 1; i <= node; i++){
-                if(graph[temp][i] == 1 && visited[i] == false){
-                    q.offer(i);
-                    visited[i] = true;
-                    System.out.print(i + " ");
-                }
-            }
-        }
-    }
-
+	static void dfs(int start) {
+		visited[start] = true;
+		System.out.print(start + " ");
+		for (int i = 1; i <= n; i++) {
+			if (g[start][i] == 0) {
+				continue;
+			}
+			if (visited[i]) {
+				continue;
+			}
+			visited[i] = true;
+			dfs(i);
+		}
+	}
+	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		node = sc.nextInt();
-		edge = sc.nextInt();
+		n = sc.nextInt();
+		g = new int[n + 1][n + 1];
+		visited = new boolean[n + 1];
+		int e = sc.nextInt();
 		start = sc.nextInt();
-		int first, second;
-		for (int i = 0; i < edge; i++) {
-			first = sc.nextInt();
-			second = sc.nextInt();
-			graph[first][second] = 1;
-			graph[second][first] = 1;
+		for (int i = 0; i < e; i++) {
+			int r = sc.nextInt();
+			int c = sc.nextInt();
+			g[r][c] = g[c][r] = 1;
 		}
-
 		dfs(start);
 		System.out.println();
-		for (int i = 1; i <= node; i++) {
-			visited[i] = false;
+		visited = new boolean[n + 1];
+		Queue<Integer> q = new LinkedList<>();
+		q.add(start);
+		visited[start] = true;
+		while(!q.isEmpty()) {
+			int now = q.poll();
+			System.out.print(now + " ");
+			for (int i = 1; i <= n; i++) {
+				if (g[now][i] == 0) {
+					continue;
+				}
+				if (visited[i]) {
+					continue;
+				}
+				q.add(i);
+				visited[i] = true;
+			}
 		}
-		bfs(start);
 	}
 }
