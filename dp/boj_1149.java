091dca7a94b51754acdac198afef06303ce66e5b
= <rightgear11@gmail.com>
2018-03-30 00:59:38 +0900

    백준, 1149, RGB거리

--- dp/boj_1149.java
+++ dp/boj_1149.java
@@ -1,27 +1,28 @@
 package practice;
 
-import java.util.Scanner;
+import java.util.*;
 
-// 백준, 1149, RGB 거리
-public class Solution {
+// 백준, 1149, RGB거리
+public class Main6 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int input = sc.nextInt();
-		int[][] grid = new int[input + 1][3 + 1];
-		for (int i = 1; i <= input; i++) {
-			for (int j = 1; j <= 3; j++) {
-				grid[i][j] = sc.nextInt();
-			}
+		int n = sc.nextInt();
+		int dp[][] = new int[n][3];
+		int color[][] = new int[n][3];
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < 3; j++) {
+				color[i][j] = sc.nextInt();
+			}			
 		}
-		int[][] dp = new int[input + 1][3 + 1];
-		for (int i = 1; i <= 3; i++) {
-			dp[0][i] = 0;
+		for (int i = 0; i < 3; i++) {
+			dp[0][i] = color[0][i];
 		}
-		for (int i = 1; i <= input; i++) {
-			dp[i][1] = Math.min(dp[i - 1][2], dp[i - 1][3]) + grid[i][1];
-			dp[i][2] = Math.min(dp[i - 1][1], dp[i - 1][3]) + grid[i][2];
-			dp[i][3] = Math.min(dp[i - 1][1], dp[i - 1][2]) + grid[i][3];
+		for (int i = 1; i < n; i++) {
+			dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + color[i][0];
+			dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + color[i][1];
+			dp[i][2] = Math.min(dp[i - 1][0], dp[i - 1][1]) + color[i][2];	
 		}
-		System.out.println(Math.min(Math.min(dp[input][1], dp[input][2]), dp[input][3]));
+		int result =Math.min(Math.min(dp[n - 1][0], dp[n - 1][1]), dp[n - 1][2]);
+		System.out.println(result);
 	}
 }
