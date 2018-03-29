54aeb81444b4089c44b3329bd10ae37822fbc4e8
= <rightgear11@gmail.com>
2018-03-29 13:42:59 +0900

    백준, 2193, 이친수(tD & bU)

--- dp/boj_2193.java
+++ dp/boj_2193.java
@@ -5,7 +5,7 @@ import java.util.*;
 // 백준, 이친수, 2193
 public class Main6 {
 	static long dp[];
-	
+
 	// topDown
 	static long func(int num) {
 		if (dp[num] != -1) {
@@ -18,12 +18,11 @@ public class Main6 {
 		dp[num] = func(num -1) + func(num - 2);
 		return dp[num];
 	}
-	
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 		dp = new long[n + 1];
-		Arrays.fill(dp, -1);
 		func(n);
 //		bottomUp
 //		for (int i = 0; i <= n; i++) {
