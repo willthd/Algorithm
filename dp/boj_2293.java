ba07696e65ee1f37757e8b27bf4e68cfaba66d50
= <rightgear11@gmail.com>
2018-04-01 00:45:57 +0900

    백준, 2293, 동전1

--- dp/boj_2293.java
+++ dp/boj_2293.java
@@ -1,26 +1,33 @@
 package practice;
 
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 // 백준, 2293, 동전1
-// 1660번 문제와 비슷
-public class Solution {
+// 전체 문제 : n개로 k원 만들 수 있는 경우의 수 : dp[n][k]
+// 대개 n과 n-1과의 관계를 설정했는데, 변수를 무엇으로 해야할지 감이 안왔다. 변수 두 개인 경우가 이렇다
+public class Main2 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int kinds = sc.nextInt();
-		int howMuch = sc.nextInt();
-		int[] coins = new int[kinds + 1];
-		for (int i = 1; i <= kinds; i++) {
+		int n = sc.nextInt();
+		int k = sc.nextInt();
+		int dp[][] = new int[n + 1][k + 1];
+		int coins[] = new int[n + 1];
+		for (int i = 1; i <= n; i++) {
 			coins[i] = sc.nextInt();
 		}
-		int[] dp = new int[howMuch + 1];
-		dp[0] = 1;
-		for (int i = 1; i <= kinds; i++) {
-			for (int j = coins[i]; j <= howMuch; j++) {
-				dp[j] += dp[j - coins[i]];
+		for (int i = 0; i <= n; i++) {
+			dp[i][0] = 1;
+		}
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= k; j++) {
+				if (j - coins[i] < 0) {
+					// 마지막 코인이 목표 가치보다 크면 그 코인을 사용하지 않을 때랑 같기 때문
+					dp[i][j] = dp[i - 1][j];
+				} else {
+					dp[i][j] = dp[i - 1][j] + dp[i][j - coins[i]];
+				}
 			}
 		}
-		System.out.println(dp[howMuch]);
+		System.out.println(dp[n][k]);
 	}
 }
