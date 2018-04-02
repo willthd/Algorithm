02ebbb3f779ba30680f1aff5527c259a67651640
= <rightgear11@gmail.com>
2018-04-02 15:33:56 +0900

    ***, 백준, 1660, 캡틴 이다솜

--- dp/boj_1660.java
+++ dp/boj_1660.java
@@ -3,7 +3,7 @@ package practice;
 import java.util.Arrays;
 import java.util.Scanner;
 
-// 백준, 1660, 캡틴 이다솜
+// 백준, 1660, 캡틴 이다솜 
 public class Main3{
     public static int sum(int input) {
       int sum = 0;
@@ -12,7 +12,7 @@ public class Main3{
       }
       return sum;
     }
-    
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
