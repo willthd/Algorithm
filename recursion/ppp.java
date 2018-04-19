7ed17502e106fd766059b5d7321149bd643b3c1c
= <rightgear11@gmail.com>
2018-04-19 21:15:19 +0900

    유클리드 호제법, 최소공배수, 최대공약수

--- recursion/ppp.java
+++ recursion/ppp.java
@@ -1,25 +1,30 @@
+package practice;
+
 import java.util.*;
 
 public class ppp {
 	
-	static long gcd(long a, long b) {
-		long max = Math.max(a, b);
-		long min = Math.min(a, b);
-		if (max % min == 0) {
-			return min;
+	static int lcm(int a, int b) {
+		if (a % b == 0) {
+			return a;
 		} else {
-			return gcd(min, max % min);
+			return a * b / gcd(a, b);
 		}
 	}
 	
-	static long lcm(long a, long b) {
-		return (a * b) / gcd(a, b);
+	static int gcd(int a, int b) {
+		if (a % b == 0) {
+			return b;
+		} else {
+			return gcd(b, a % b);
+		}
 	}
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int m = sc.nextInt();
-		System.out.println(lcm(n, m));
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		System.out.println(gcd(a, b));
+		System.out.println(lcm(a, b));
 	}
 }
