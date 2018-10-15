3bf8b2392a025a6c35394639fb75aedafba012c4
willthd <rightgear11@gmail.com>
2018-10-15 18:45:22 +0900

    백준, 15686, 치킨 배달

--- bruteForce/boj_15686.java
+++ bruteForce/boj_15686.java
@@ -1,25 +1,63 @@
-package practice;
-
 import java.util.*;
 
-public class Main5 {
-	static int a, b, c;
+// bruteForce, 조합
+public class Main {
+	static int n, m, total = 0, result = Integer.MAX_VALUE;
+	static List<Node> per, chi;
+	static int a[];
 
-	static long f(long a, long b) {
-		if (b == 0) {
-			return 1;
-		} else if (b % 2 == 0) {
-			return (long) ((long)Math.pow((long)f(a, b / 2), 2) % c);
-		} else {
-			return (long) ((long)Math.pow((long)f(a, b / 2), 2) * (a % c)) % c;
+	public static void f(int st, int dep) {
+		if (dep == m) {
+			total = 0;
+			for (int i = 0; i < per.size(); i++) {
+				int min_dist = Integer.MAX_VALUE;
+				for (int j = 0; j < m; j++) {
+					int dist = getDist(per.get(i), chi.get(a[j]));
+					min_dist = Math.min(dist, min_dist);
+				}
+				total += min_dist;
+			}
+			result = Math.min(result, total);
+			return;
 		}
+		for (int i = st; i < chi.size(); i++) {
+			a[dep] = i;
+			f(i + 1, dep + 1);
+		}
+	}
+
+	public static int getDist(Node per, Node chi) {
+		return (int) (Math.abs(per.x - chi.x) + Math.abs(per.y - chi.y));
 	}
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		a = sc.nextInt();
-		b = sc.nextInt();
-		c = sc.nextInt();
-		System.out.println(f(a, b));
+		n = sc.nextInt();
+		m = sc.nextInt();
+		per = new LinkedList<>();
+		chi = new LinkedList<>();
+		a = new int[m];
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				int a = sc.nextInt();
+				if (a == 2) {
+					chi.add(new Node(i, j));
+				} else if (a == 1) {
+					per.add(new Node(i, j));
+				}
+			}
+		}
+		f(0, 0);
+		System.out.println(result);
+	}
+}
+
+class Node {
+	int x;
+	int y;
+
+	Node (int x, int y) {
+		this.x = x;
+		this.y = y;
 	}
 }
