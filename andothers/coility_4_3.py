13452699c2e687330251cf42f63d3eba359a7e0d
willthd <rightgear11@gmail.com>
2021-07-25 16:37:53 +0900

    MissingInteger : easy solving

--- andothers/coility_4_3.py
+++ andothers/coility_4_3.py
@@ -1,10 +1,9 @@
-from collections import Counter
-
 def solution(A):
-    positive_A = list(filter(lambda x: x > 0, A))
-    counter = Counter(positive_A)
-    sorted_cnt = sorted(list(counter.keys()))
-    for i, e in enumerate(sorted_cnt):
-        if (i+1) != e:
-            return (i+1)
-    return (len(sorted_cnt)+1)
+    from collections import Counter
+    cnt = Counter(A)
+
+    for i in range(1, len(A)+1):
+        if i not in cnt:
+            return i
+
+    return len(A)+1
